ae834db8d086a3bd0487052310465b2368b6ed75
marcus <marcus.apjr@gmail.com>
2021-06-12 20:37:24 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,36 @@
+var baixou = true;
+var img = document.createElement("img");
+
+
+function botao(){
+    if (baixou) {
+        happy();
+    } else {
+        sad();
+    }
+
+}
+
+function happy(){
+    console.log("baixou");
+    document.getElementById("resposta").innerHTML = "Baixou! :D" ;
+    img.src = "media/free.gif";
+    var srcI = document.getElementById("imagem");
+    srcI.appendChild(img);
+    var audio = document.createElement("audio");
+    audio.src = "media/happy.mp3";
+    audio.play();
+}
+
+function sad() {
+    console.log("não baixou");
+    document.getElementById("resposta").innerHTML = "Não baixou :(" ;
+    img.src = "media/sad-lego-pirate.jpg";
+    var srcI = document.getElementById("imagem");
+    srcI.appendChild(img);
+    var audio = document.createElement("audio");
+    audio.src = "media/sad.mp3";
+    audio.play();
+        
+}
+
